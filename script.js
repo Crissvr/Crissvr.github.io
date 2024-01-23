fb42e08935d1f109f8d8d8dfb60a2b531ffe372d
Crissvr <chrisvalrot@gmail.com>
2024-01-22 19:56:01 -0500

    add skills and mailto

--- script.js
+++ script.js
@@ -11,16 +11,22 @@ function scrollToElement(elementSelector, instance = 0){
 const link1 = document.getElementById("link1")
 const link2 = document.getElementById("link2")
 const link3 = document.getElementById("link3")
+const link4 = document.getElementById("link4")
 
 link1.addEventListener('click', () =>{
     scrollToElement('.header');
 });
 
 link2.addEventListener('click', () =>{
-    //Scroll to the secpnd element with "header" class
+    //Scroll to the second element with "header" class
     scrollToElement('.header', 1);
 });
 
 link3.addEventListener('click', () =>{
+    //Scroll to the thrid element with "header" class
+    scrollToElement('.header', 2);
+});
+
+link4.addEventListener('click', () =>{
     scrollToElement('.column');
 });
